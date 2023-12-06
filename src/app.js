b23cc201ac8376f5d0f4095f9f19190160afbc8b
Zack Newman <znewman@indeed.com>
2023-12-06 13:08:23 -0800

    Actually require cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 if (process.env.USER) require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router");
